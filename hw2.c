ea8db60db1df42371bbd5bdedb19b9cbef412ec3
joshm <jmaitoza@umassd.edu>
2022-03-30 16:36:53 -0400

    finished assignment

--- hw2.c
+++ hw2.c
@@ -6,19 +6,26 @@
 #define MIN_PID 300
 #define MAX_PID 5000
 
-// int in_use[MAX_PID + 1];
+/* a header file would not work for this homework assignment.
+ * However, if it did, my pid_1.h file would include the following functions:
+ * allocate_map(void)
+ * allocate_pid(void)
+ * release_pid(int pid) 
+ */
+
+
 char available_pids[SIZE];
 
+// Initiallizes an array of zeros, with zero representing an available pid for use 
 int allocate_map(void) {
-  // char available_pids[SIZE];
-  int pid_num = 300;
   for (int i = 0; i < SIZE; i++) {
     available_pids[i] = 0;
-    pid_num++;
   }
   return 1;
 }
 
+// Checks for the first available pid in the array and marks it as unavailable.
+// It then returns the index plus the offset of 300 as the allocated pid
 int allocate_pid(void) {
   for (int i = 0; i < SIZE; i++) {
     if (available_pids[i] == 0) {
@@ -37,13 +44,13 @@ int release_pid(int pid) {
 int main (int argc, char *argv[])
 {
   int i, count;
-  srandom(100);
-
+  int mypid;
+  
   allocate_map();
 
   for (i = 0; i < 1000; i++) {
-    available_pids[i] = allocate_pid();
-    printf("PID: %d\n", available_pids[i]);
+    mypid = allocate_pid(); // use function to allocate a pid to mypid variable
+    printf("PID: %d\n", mypid); // print out said pid
     if (available_pids[i] == -1) {
       break;
     }
@@ -55,7 +62,9 @@ int main (int argc, char *argv[])
   i = 0;
   while (count < 500) {
     if (available_pids[i]) {
-      release_pid(available_pids[i]);
+      release_pid(count+300); //release the pid at the offset
+      printf("PID: %d was released\n", count+300);
+      printf("status at index: %d\n", available_pids[count]);
       count++;
     }
     i++;
